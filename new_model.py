f4ef414f3c21857c67ae8272e32306ab2e7ce388
Andrew <yanqingbao@LeGuan.local>
2020-10-15 12:54:24 -0500

    new_model

--- new_model.py
+++ new_model.py
@@ -1,11 +1,13 @@
 # Using Pegasus-cnn_dailymail for abstractive summarization
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 import torch
+import streamlit as st
 
-# src_text = [
-#     """ PG&E stated it scheduled the blackouts in response to forecasts for high winds amid dry conditions. The aim is to reduce the risk of wildfires. Nearly 800 thousand customers were scheduled to be affected by the shutoffs which were expected to last through at least midday tomorrow."""
-# ]
+src_text = [
+    """ PG&E stated it scheduled the blackouts in response to forecasts for high winds amid dry conditions. The aim is to reduce the risk of wildfires. Nearly 800 thousand customers were scheduled to be affected by the shutoffs which were expected to last through at least midday tomorrow."""
+]
 
+@st.cache
 def pega_sum(src_text):
     src_text = [src_text]
     model_name = 'google/pegasus-cnn_dailymail'
@@ -22,3 +24,20 @@ def pega_sum(src_text):
 
 # sum_text = pega_sum(src_text)
 # print(sum_text)
+
+@st.cache
+def pega_xsum(src_text):
+    src_text = [src_text]
+    model_name = 'google/pegasus-xsum'
+    torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    tokenizer = PegasusTokenizer.from_pretrained(model_name)
+    model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+    batch = tokenizer.prepare_seq2seq_batch(src_text, truncation=True, padding='longest').to(torch_device)
+    translated = model.generate(**batch)
+    tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+
+    return tgt_text[0]
+
+
+sum_text = pega_xsum(src_text[0])
+print(sum_text)
